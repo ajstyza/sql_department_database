e8b5b97c2065396b746205d701e77b5106decb81
ajstyza <124314058+ajstyza@users.noreply.github.com>
2023-06-11 13:14:00 -0400

    added drop

--- schema.sql
+++ schema.sql
@@ -1,8 +1,9 @@
--- CREATE DATABASE department_db;
+DROP DATABASE IF EXISTS department_db;
+CREATE DATABASE department_db;
 
 USE department_db;
 
-CREATE TABLE departments (
+ CREATE TABLE departments (
    id INT NOT NULL,
    department_name VARCHAR(30) NOT NULL,
 
@@ -10,4 +11,4 @@ CREATE TABLE departments (
 
 
 
--- SELECT DATABASE();
+SELECT DATABASE();
