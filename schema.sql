4d500ca21e1efcf8272c274458d0c7dd5da8cc67
ajstyza <124314058+ajstyza@users.noreply.github.com>
2023-06-11 13:06:11 -0400

    added stuff

---
+++ schema.sql
@@ -0,0 +1,13 @@
+-- CREATE DATABASE department_db;
+
+USE department_db;
+
+CREATE TABLE departments (
+   id INT NOT NULL,
+   department_name VARCHAR(30) NOT NULL,
+
+);
+
+
+
+-- SELECT DATABASE();
