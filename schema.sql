a220b0daf101094e771055ed99f1c2b20fd22db5
ajstyza <124314058+ajstyza@users.noreply.github.com>
2023-06-12 18:50:54 -0400

    added some notes and keys are fixed

--- schema.sql
+++ schema.sql
@@ -5,35 +5,64 @@ CREATE DATABASE department_db;
 
 USE department_db;
 
+-- CREATE TABLE departments (
+--    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--    department_name VARCHAR(30) UNIQUE NOT NULL,
+--    -- ON DELETE SET NULL
+-- );
+
+-- CREATE TABLE roles (
+--    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+--    title VARCHAR(30) UNIQUE NOT NULL,
+--    salaray DECIMAL UNSIGNED NOT NULL,
+--    department_id INT NOT NULL,
+--    FOREIGN KEY(department_id),
+--    REFERENCES departments(id),
+--    PRIMARY KEY(id)
+  
+-- );
+
+-- CREATE TABLE employees (
+--    employee_id INT NOT NULL AUTO_INCREMENT,
+--    first_name VARCHAR(30),
+--    last_name VARCHAR(30),
+--    role_id INT NOT NULL,
+--    job_title VARCHAR(30),
+--    manager_id INT NOT NULL,
+--    FOREIGN KEY(role_id),
+--    FOREIGN KEY(manager_id),
+--    REFERENCES roles(id),
+--    REFERENCES roles(department_id)
+--    -- ON DELETE SET NULL
+-- );
+
 CREATE TABLE departments (
-   id INT NOT NULL AUTO_INCREMENT,
-   department_name VARCHAR(30) NOT NULL,
-   PRIMARY KEY(id),
-   -- ON DELETE SET NULL
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  department_name VARCHAR(30) UNIQUE NOT NULL
 );
+
 
 CREATE TABLE roles (
-   id INT NOT NULL AUTO_INCREMENT,
-   title VARCHAR(30),
-   salaray DECIMAL,
-   department_id INT NOT NULL,
-   FOREIGN KEY(department_id),
-   REFERENCES departments(id),
-   PRIMARY KEY(id)
-  
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL UNSIGNED NOT NULL,
+  department_id INT UNSIGNED NOT NULL,
+  INDEX dep_ind (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE CASCADE
 );
 
+
 CREATE TABLE employees (
-   employee_id INT NOT NULL AUTO_INCREMENT,
-   first_name VARCHAR(30),
-   last_name VARCHAR(30),
-   role_id VARCHAR(30),
-   job_title INT NOT NULL,
-   manager_id INT NOT NULL,
-   FOREIGN KEY(role_id),
-   FOREIGN KEY(manager_id),
-   REFERENCES roles(id),
-   REFERENCES roles(department_id)
-   -- ON DELETE SET NULL
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT UNSIGNED NOT NULL,
+  INDEX role_ind (role_id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
+  manager_id INT UNSIGNED,
+  INDEX man_ind (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
+
+
 SELECT DATABASE();
--- seeds.sql
+++ seeds.sql
@@ -1,25 +1,48 @@
-INSERT INTO departments (id, department_name)
+-- INSERT INTO departments (id, department_name)
+-- VALUES 
+-- (001, "Humanities"),
+-- (002, "Science"),
+-- (003, "Medicine"),
+-- (004, "Athletics");
+
+-- INSERT INTO roles (id, title, salary)
+-- VALUES
+-- (001, "professor", 80000),
+-- (002, "lecturer", 50000),
+-- (003, "TA", 35000),
+-- (004, "head coach", 100000),
+-- (005, "assistant coach", 80000),
+-- (006, "department chair", 200,000);
+
+
+-- INSERT INTO employees (employee_id, first_name, last_name, role_id, job_title, department_name, manager_id)
+-- VALUES
+-- (001, "Mark", "Hamil"),
+-- (002, "Reginald", "Horseman"),
+-- (003, "Ricky", "Ricardo"),
+-- (004, "Mark", "Bolan");
+
+
+INSERT INTO departments (department_name)
 VALUES 
-(001, "Humanities"),
-(002, "Science"),
-(003, "Medicine"),
-(004, "Athletics");
+("Humanities"),
+("Science"),
+("Medicine"),
+( "Athletics");
 
-INSERT INTO roles (id, title, salary)
+INSERT INTO roles (title, salary, department_id)
 VALUES
-(001, "professor", 80000),
-(002, "lecturer", 50000),
-(003, "TA", 35000),
-(004, "head coach", 100000),
-(005, "assistant coach", 80000),
-(006, "department chair", 200,000);
+("professor", 80000, 1),
+("lecturer", 50000, 1),
+("TA", 35000, 1),
+("head coach", 100000, 1),
+("assistant coach", 80000, 1),
+("department chair", 200000, 1);
 
 
-INSERT INTO employees (employee_id, first_name, last_name, role_id, job_title, department_name, manager_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
-(001, "Mark", "Hamil"),
-(002, "Reginald", "Horseman"),
-(003, "Ricky", "Ricardo"),
-(004, "Mark", "Bolan");
-
-
+("Mark", "Hamil", 1, NULL),
+("Reginald", "Horseman", 2, 1),
+("Ricky", "Ricardo", 3, 1),
+("Mark", "Bolan", 4, 1);
