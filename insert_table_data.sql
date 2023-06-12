c42f5f244da0775fc490b1ce3c2b1afa01bf8627
ajstyza <124314058+ajstyza@users.noreply.github.com>
2023-06-11 21:20:02 -0400

    added tables

---
+++ insert_table_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO departments (id, department_name)
+VALUES 
+(001, "Humanities"),
+(002, "Science"),
+(003, "Medicine"),
+(004, "Athletics");
--- schema.sql
+++ schema.sql
@@ -3,11 +3,31 @@ CREATE DATABASE department_db;
 
 USE department_db;
 
- CREATE TABLE departments (
+CREATE TABLE departments (
    id INT NOT NULL,
-   department_name VARCHAR(30) NOT NULL,
+   department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+   professor 
+   lecturer
+   TA 
 
 );
+
+CREATE TABLE employees (
+   employee_id INT NOT NULL,
+   employee_name VARCHAR(30),
+   employee_role VARCHAR(30),
+   job_title VARCHAR(30),
+   department_name VARCHAR(30),
+   salary FLOAT(6),
+   manager VARCHAR(30)
+);
+
+CREATE TABLE salary (
+   
+)
 
 
 
