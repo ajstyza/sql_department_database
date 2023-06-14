50594672200eec1c168dfcf1b283dcb4e095bd78
ajstyza <124314058+ajstyza@users.noreply.github.com>
2023-06-14 13:18:59 -0400

    added queries

--- schema.sql
+++ schema.sql
@@ -5,37 +5,6 @@ CREATE DATABASE department_db;
 
 USE department_db;
 
--- CREATE TABLE departments (
---    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
---    department_name VARCHAR(30) UNIQUE NOT NULL,
---    -- ON DELETE SET NULL
--- );
-
--- CREATE TABLE roles (
---    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
---    title VARCHAR(30) UNIQUE NOT NULL,
---    salaray DECIMAL UNSIGNED NOT NULL,
---    department_id INT NOT NULL,
---    FOREIGN KEY(department_id),
---    REFERENCES departments(id),
---    PRIMARY KEY(id)
-  
--- );
-
--- CREATE TABLE employees (
---    employee_id INT NOT NULL AUTO_INCREMENT,
---    first_name VARCHAR(30),
---    last_name VARCHAR(30),
---    role_id INT NOT NULL,
---    job_title VARCHAR(30),
---    manager_id INT NOT NULL,
---    FOREIGN KEY(role_id),
---    FOREIGN KEY(manager_id),
---    REFERENCES roles(id),
---    REFERENCES roles(department_id)
---    -- ON DELETE SET NULL
--- );
-
 CREATE TABLE departments (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   department_name VARCHAR(30) UNIQUE NOT NULL
@@ -66,3 +35,34 @@ CREATE TABLE employees (
 
 
 SELECT DATABASE();
+
+-- CREATE TABLE departments (
+--    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--    department_name VARCHAR(30) UNIQUE NOT NULL,
+--    -- ON DELETE SET NULL
+-- );
+
+-- CREATE TABLE roles (
+--    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+--    title VARCHAR(30) UNIQUE NOT NULL,
+--    salaray DECIMAL UNSIGNED NOT NULL,
+--    department_id INT NOT NULL,
+--    FOREIGN KEY(department_id),
+--    REFERENCES departments(id),
+--    PRIMARY KEY(id)
+  
+-- );
+
+-- CREATE TABLE employees (
+--    employee_id INT NOT NULL AUTO_INCREMENT,
+--    first_name VARCHAR(30),
+--    last_name VARCHAR(30),
+--    role_id INT NOT NULL,
+--    job_title VARCHAR(30),
+--    manager_id INT NOT NULL,
+--    FOREIGN KEY(role_id),
+--    FOREIGN KEY(manager_id),
+--    REFERENCES roles(id),
+--    REFERENCES roles(department_id)
+--    -- ON DELETE SET NULL
+-- );
